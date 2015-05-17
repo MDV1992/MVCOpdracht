9bc17600b4d5e4a2cefac8af41e7ac06557f6673
Maarten De Vleeschouwer <maarten.devleeschouwer@gmail.com>
2015-05-17 10:58:45 +0200

    V1.1

---
+++ MVCOpdracht/Models/Component.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCOpdracht.Models
+{
+    public class Component
+    {
+        int ID { get; set; }
+
+        string Categorie { get; set; }
+
+        string Naam { get; set; }
+        string Link { get; set; }
+        int Aantal { get; set; }
+        double Prijs { get; set; }
+
+    }
+}
